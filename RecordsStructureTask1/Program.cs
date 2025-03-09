f7cebe4de5f94508423b4cc8a953fb75ce0b8e77
Sandr0ta <144434115+Sandr0ta@users.noreply.github.com>
2025-03-09 20:34:59 +0000

    Update Program.cs

--- RecordsStructureTask1/Program.cs
+++ RecordsStructureTask1/Program.cs
@@ -24,14 +24,38 @@
             Film ironMan = new Film("Iron Man","12A",2008,false);
             Film antMan = new Film("Ant-Man", "12A", 2015, false);
             Film[] filmCollection = new Film[] { antMan,hulk,ironMan };
-            int year = 0;
-            int position = 0;
+
 
             //  Write the code to do the following
             // Loop through the array of films and check for the newest film
             // Produce one output to say the name of the newest film
 
             // Write the code to update the andMan record to show the film is currently being shown
+
+                        Film newestFilm = filmCollection[0];
+            foreach (Film film in filmCollection)
+            {
+                if (film.year > newestFilm.year)
+                {
+                    newestFilm = film;
+                }
+            }
+            Console.WriteLine($"The newest film is: {newestFilm.title}");
+
+            // Updating Ant-Man to show it is currently being shown
+            for (int i = 0; i < filmCollection.Length; i++)
+            {
+                if (filmCollection[i].title == "Ant-Man")
+                {
+                    filmCollection[i] = new Film(filmCollection[i].title, filmCollection[i].certificate, filmCollection[i].year, true);
+                }
+            }
+
+            // Checking if Ant-Man is now being shown
+            foreach (Film film in filmCollection)
+            {
+                Console.WriteLine($"{film.title} - Being shown: {film.beingShown}");
+            }
 
 
         }
